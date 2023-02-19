5bb47644e1e4bfdc4e68d86d40fd3ba1e1a95f32
weekkkk <nikitanedelko9@gmail.com>
2023-02-19 03:48:26 +0200

    complite

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ import router from './router';
 
 import './assets/style/index.scss';
 
-// @ts-ignore
-import VueHighlightJS from 'vue3-highlightjs';
-import 'highlight.js/styles/atom-one-dark.css';
-
 const app = createApp(App);
 
-app.use(VueHighlightJS).use(router).mount('#app');
+app.use(router).mount('#app');
