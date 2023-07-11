6d89b379d016cc23ab0a256bf1695f953f9f0a8d
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2023-07-11 04:50:09 +0300

    app gh-pages

--- src/pages/landing-page/index.ts
+++
@@ -1,1 +0,0 @@
-export const LandingPage = () => import('./LandingPage.vue')
--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cornelius',
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    },
+  },
 })
