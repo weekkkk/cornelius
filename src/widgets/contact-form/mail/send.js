1fcc534a1792d0886be928cd7f01be63b4441f61
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2023-07-16 01:43:07 +0300

    fix email for contact form

--- src/widgets/contact-form/mail/send.js
+++ src/widgets/contact-form/mail/send.js
@@ -4,13 +4,13 @@ export const send = async (contact) => {
     <br>
     <h1>Имя: </h1>${contact.name || '-'}
     <br>
-    <h1>Сообщение: </h1>${contact.name || '-'}
+    <h1>Сообщение: </h1>${contact.message || '-'}
     `
 
   const message = await Email.send({
-    SecureToken: '4ccadafe-75fb-4dea-b418-bd3f0ab5a041', //add your token here
-    To: 'nikita.nedelko.job@gmail.com',
-    From: 'nikita.nedelko.job@gmail.com',
+    SecureToken: '339c8ec4-f6d1-4536-a646-a7f7cef2abeb', //add your token here
+    To: 'cornelius.media5@gmail.com',
+    From: 'cornelius.media5@gmail.com',
     Subject: `Заяка на звонок с corenelius.md ${contact.phone}`,
     Body: ebody
   })
