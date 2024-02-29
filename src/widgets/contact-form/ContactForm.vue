16815225b294a43b46b580f6cad5fa7cebd34d51
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2024-02-29 21:05:30 +0200

    fix

--- src/widgets/contact-form/ContactForm.vue
+++ src/widgets/contact-form/ContactForm.vue
@@ -1,11 +1,43 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { computed, onMounted, ref } from 'vue'
+import type { ContactType } from './types'
+import { reactive } from 'vue'
+import { MailService } from '@/shared'
+import { send } from './mail'
+
+const contact: ContactType = reactive({
+  phone: '',
+  name: '',
+  message: ''
+})
+
+const isLoading = ref(false)
+
+const sendMail = async () => {
+  isLoading.value = true
+
+  await send(contact)
+
+  isLoading.value = false
+
+  emit('success')
+}
+
+const emit = defineEmits<{
+  (e: 'success'): void
+}>()
+
+const phoneError = computed(() => {
+  if (!contact.phone) return 'Номер телефона является обязательным полем формы'
+})
+</script>
 
 <template>
   <form action="" class="contact_form f fd-col rg-4">
     <input class="contact_form-input" type="text" placeholder="Ваш телефон" />
     <input class="contact_form-input" type="text" placeholder="Ваше имя" />
     <input class="contact_form-input" type="text" placeholder="Ваше сообщение" />
-    <button>Отправить</button>
+    <button :disabled="isLoading || !!phoneError">Отправить</button>
   </form>
 </template>
 
